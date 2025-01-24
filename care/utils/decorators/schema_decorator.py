de73f58f2bf9f7cfa9edfda299f19f7a23be1c15
Prafful <praffulsharma1230@gmail.com>
2025-01-24 15:13:50 +0530

    updated the decorator

--- care/utils/decorators/schema_decorator.py
+++ care/utils/decorators/schema_decorator.py
@@ -2,23 +2,25 @@ from drf_spectacular.utils import extend_schema, extend_schema_view
 
 
 def generate_swagger_schema_decorator(cls):
-    schema_dict = {}
-    for name in ["create", "update", "list", "retrieve"]:
-        if name == "create":
-            schema_dict["create"] = extend_schema(
-                request=cls.pydantic_model,
-                responses={200: cls.pydantic_read_model or cls.pydantic_model},
-            )
-        elif name == "update":
-            schema_dict["update"] = extend_schema(
-                request=cls.pydantic_retrieve_model
-                or cls.pydantic_read_model
-                or cls.pydantic_model,
-                responses={200: cls.pydantic_read_model or cls.pydantic_model},
-            )
-        elif name in ["list", "retrieve"]:
-            schema_dict[name] = extend_schema(
-                responses={200: cls.pydantic_read_model or cls.pydantic_model}
-            )
+    actions = {
+        "create": {
+            "request": cls.pydantic_model,
+            "responses": {200: cls.pydantic_read_model or cls.pydantic_model},
+        },
+        "update": {
+            "request": cls.pydantic_retrieve_model
+            or cls.pydantic_read_model
+            or cls.pydantic_model,
+            "responses": {200: cls.pydantic_read_model or cls.pydantic_model},
+        },
+        "list": {"responses": {200: cls.pydantic_read_model or cls.pydantic_model}},
+        "retrieve": {"responses": {200: cls.pydantic_read_model or cls.pydantic_model}},
+    }
+
+    schema_dict = {
+        name: extend_schema(**params)
+        for name, params in actions.items()
+        if hasattr(cls, name) and callable(getattr(cls, name))
+    }
 
     return extend_schema_view(**schema_dict)(cls)
