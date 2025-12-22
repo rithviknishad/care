e6571ea1e90ea18df1a3b214bde40f258de53673
rithviknishad <mail@rithviknishad.dev>
2025-12-22 14:56:45 +0530

    add validate datapoints

--- care/emr/management/commands/import_inventory_items.py
+++ care/emr/management/commands/import_inventory_items.py
@@ -1,7 +1,12 @@
 import logging
 
 from django.core.management.base import BaseCommand
+from django.db import transaction
 
+from care.emr.resources.charge_item_definition.spec import ChargeItemDefinitionWriteSpec
+from care.emr.resources.inventory.product_knowledge.spec import (
+    ProductKnowledgeWriteSpec,
+)
 from care.emr.resources.resource_category.spec import ResourceCategoryWriteSpec
 
 logger = logging.getLogger(__name__)
@@ -16,9 +21,31 @@ class Command(BaseCommand):
         super().__init__(*args, **options)
         data_dir = args[1]
         # TODO: read all the json files
-        pass
 
-    def validate_data(self, resource_categories: list, product_knowledges: list):
-        for resource_category in resource_categories:
-            instance = ResourceCategoryWriteSpec.model_validate(resource_category)
-            model_instance = instance.de_serialize()
+        resource_category_instances = self.validate_datapoints(
+            ResourceCategoryWriteSpec,
+            [],  # TODO: replace this
+        )
+        product_knowledge_instances = self.validate_datapoints(
+            ProductKnowledgeWriteSpec,
+            [],  # TODO: replace this
+        )
+        charge_item_definition_instances = self.validate_datapoints(
+            ChargeItemDefinitionWriteSpec,
+            [],  # TODO: replace this
+        )
+
+        with transaction.atomic():
+            for instance in resource_category_instances:
+                instance.save()
+            for instance in product_knowledge_instances:
+                instance.save()
+            for instance in charge_item_definition_instances:
+                instance.save()
+
+    def validate_datapoints(self, spec, datapoints):
+        model_instances = []
+        for datapoint in datapoints:
+            instance = spec.model_validate(datapoint)
+            model_instances.append(instance.de_serialize())
+        return model_instances
